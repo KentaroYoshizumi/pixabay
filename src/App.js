5c6aec4cb007902a7800867c4c8d3aec717377ae
Kentaro Yoshizumi <kentaroyoshizumi@mbp.local>
2021-05-23 06:38:12 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit = async (term) => {
+    try {
+      const params={
+        key: "21735578-1ec536aab72c01c4977129bbd",
+        q: term,
+      };
+      const response = await axios.get("https://pixabay.com/api/",{params});
+      setImages(response.data.hits);
+      if(response.data.total===0){
+        window.alert('お探しの画像はありません。');
+      }
+    }catch{
+     window.alert('写真の取得に失敗しました。');
+    }
   };
   return (
     <div className='ui container' style={{marginTop: '20px'}}>
       <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
 };
